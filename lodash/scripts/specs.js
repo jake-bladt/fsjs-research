a4782c8be19d0e58a40a9ea81798f8f22f2c93ee
Jake <jake@jakebladt.com>
2014-08-26 14:02:47 -0400

    walker

--- lodash/scripts/specs.js
+++ lodash/scripts/specs.js
@@ -29,4 +29,48 @@ describe("Lodash array helper", function() {
     expect(ndx).toEqual(1);
 
   });
+
+  it("finds the index of the last matching object in an array", function() {
+    var stepsData = [
+      { 'datestamp': '20140101', 'stepCount': 3456 },
+      { 'datestamp': '20140102', 'stepCount': 7122 },
+      { 'datestamp': '20140103', 'stepCount': 1005 },
+      { 'datestamp': '20140104', 'stepCount': 6121 },
+      { 'datestamp': '20140105', 'stepCount': 7255 }
+    ];
+
+    var ndx = _.findLastIndex(stepsData, function(sd) {
+      return sd.stepCount > 7000;
+    });
+
+    expect(ndx).toEqual(4);
+
+  });
+
+});
+
+describe("Walker app", function() {
+  it("exists", function() {
+    expect(walkerApp).toBeDefined();
+  });
+});
+
+describe("Steps analyzer", function() {
+  var sampleData = [
+      { 'datestamp': '20140101', 'stepCount':  3456 },
+      { 'datestamp': '20140102', 'stepCount':  7122 },
+      { 'datestamp': '20140103', 'stepCount':  1005 },
+      { 'datestamp': '20140104', 'stepCount':  6121 },
+      { 'datestamp': '20140105', 'stepCount':  7255 },
+      { 'datestamp': '20140106', 'stepCount':  7255 },
+      { 'datestamp': '20140107', 'stepCount':     0 },
+      { 'datestamp': '20140108', 'stepCount': 12234 },
+      { 'datestamp': '20140109', 'stepCount':  6367 },
+      { 'datestamp': '20140110', 'stepCount':  7478 }
+    ];
+
+  it("exists", function() {
+    expect(walkerApp.stepsAnalyzer).toBeDefined();
+  });
+
 });
---
+++ lodash/scripts/walker.js
@@ -0,0 +1,5 @@
+var walkerApp = function() {};
+
+(function(app) {
+	
+})(walkerApp);
