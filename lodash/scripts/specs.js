8dd00693f2ccf64241cc60d18f5c60849b088ed6
Jake Bladt <jake@jakebladt.com>
2014-08-26 21:27:12 -0400

    numeric datestamps

--- lodash/scripts/specs.js
+++ lodash/scripts/specs.js
@@ -52,16 +52,16 @@ describe("Walker app", function() {
 
 describe("Steps analyzer", function() {
   var sampleData = [
-      { 'datestamp': '20140101', 'stepCount':  3456 },
-      { 'datestamp': '20140102', 'stepCount':  7122 },
-      { 'datestamp': '20140103', 'stepCount':  1005 },
-      { 'datestamp': '20140104', 'stepCount':  6121 },
-      { 'datestamp': '20140105', 'stepCount':  7255 },
-      { 'datestamp': '20140106', 'stepCount':  8555 },
-      { 'datestamp': '20140107', 'stepCount':     0 },
-      { 'datestamp': '20140108', 'stepCount': 12234 },
-      { 'datestamp': '20140109', 'stepCount':  6367 },
-      { 'datestamp': '20140110', 'stepCount':  7478 }
+      { 'datestamp': 20140101, 'stepCount':  3456 },
+      { 'datestamp': 20140102, 'stepCount':  7122 },
+      { 'datestamp': 20140103, 'stepCount':  1005 },
+      { 'datestamp': 20140104, 'stepCount':  6121 },
+      { 'datestamp': 20140105, 'stepCount':  7255 },
+      { 'datestamp': 20140106, 'stepCount':  8555 },
+      { 'datestamp': 20140107, 'stepCount':     0 },
+      { 'datestamp': 20140108, 'stepCount': 12234 },
+      { 'datestamp': 20140109, 'stepCount':  6367 },
+      { 'datestamp': 20140110, 'stepCount':  7478 }
     ];
 
   it("exists", function() {
